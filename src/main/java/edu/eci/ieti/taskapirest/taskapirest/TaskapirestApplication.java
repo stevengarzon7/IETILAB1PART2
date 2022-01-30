8dd4f72e89d39b916030397e255ac81ef4d5b5b4
Steven Garzon <steven.garzon@mail.escuelaing.edu.co>
2022-01-30 18:35:42 -0500

    Inicializando repositorio

---
+++ src/main/java/edu/eci/ieti/taskapirest/taskapirest/TaskapirestApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.taskapirest.taskapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskapirestApplication.class, args);
+	}
+
+}
